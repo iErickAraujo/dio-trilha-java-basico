3c38850e1f0d84430445c4c43886e162c18f12e5
Erick Araujo <erickaraujo4984@gmail.com>
2023-10-12 22:55:03 -0300

    Escopo,Palavras reservadas,JavaDocs, Terminal,Args

---
+++ tipos-variaveis/src/Escopo.java
@@ -0,0 +1,35 @@
+
+public class Escopo {
+    // Escopo de classe (variável de instância)
+    int variavelDeInstancia;
+
+    public void meuMetodo() {
+        // Escopo de método
+        int variavelLocal;
+
+        if (true/* condicao */) {
+            // Escopo de bloco
+            int variavelBloco;
+
+            // Pode acessar variavelLocal, variavelBloco e variavelDeInstancia aqui
+        }
+
+        // Pode acessar variavelLocal e variavelDeInstancia aqui
+        // Não pode acessar variavelBloco aqui
+    }
+
+    public void meuOutroMetodo(int parametro) {
+        // Escopo de parâmetro
+        // Pode acessar parametro e variavelDeInstancia aqui
+    }
+
+    public static void main(String[] args) {
+        // Aqui você pode criar instâncias da classe e acessar seus métodos e variáveis
+        Escopo objeto = new Escopo();
+        objeto.meuMetodo();
+        objeto.meuOutroMetodo(42);
+
+        // Não é possível acessar variavelLocal e variavelBloco aqui, pois estão no
+        // escopo dos métodos
+    }
+}
